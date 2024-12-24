b136b70243686e05f26a76a39117abfd3f78d63e
hongky06 <dohongky2006@gmail.com>
2024-12-25 00:30:00 +0700

    Add files via upload

---
+++ 21.4.cpp
@@ -0,0 +1,41 @@
+#include<stdio.h>
+int main(){
+	int dong;
+	char str[100];
+	printf("hay nhap so luong dong: "); 
+	scanf("%d",&dong);
+	getchar();
+	FILE *nhap;
+	nhap = fopen("bt05.txt" , "w");
+	if(nhap == NULL){
+		printf("mo full ko thanh cong");
+		return 1;
+	}
+	for(int i = 0 ;i < dong; i++ ){
+		printf("%d : ",i+1);
+		fgets(str,sizeof(str) , stdin);
+		fputs(str,nhap);
+	}
+	fclose(nhap);
+	
+	nhap = fopen("bt05.txt" , "r");
+	if(nhap == NULL){
+		printf("mo full ko thanh cong");
+		return 1;
+	}
+	if (fgets(str, sizeof(str), nhap) != NULL) {
+        printf("dong dau tien trong file: %s", str);
+}
+	
+	
+	fclose(nhap);
+	
+	
+	
+	
+	
+	
+	
+	
+	return 0;
+}
